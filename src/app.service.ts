dcd2713c10bd56d82491921afe975440dfe0596d
gjm1128 <sgcs20111586@gmail.com>
2022-03-24 23:11:39 +0900

    .env configuration added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {
+    const url = this.configService.get('API_URL');
+    console.log(url);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
